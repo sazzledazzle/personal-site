02c8b201826e07a7e028b3f2524ba06ee3a11bc9
sazzledazzle <sarahclements@hotmail.co.uk>
2022-10-25 03:11:16 +0100

    updated .env variables

--- src/contentful.js
+++ src/contentful.js
@@ -1,8 +1,8 @@
 import { createClient } from "contentful";
 
 const client = createClient({
-  space: process.env.SPACE_ID,
-  accessToken: process.env.ACCESS_TOKEN,
+  space: process.env.VITE_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.VITE_CONTENTFUL_ACCESS_TOKEN,
 });
 
 const getProjects = () =>
